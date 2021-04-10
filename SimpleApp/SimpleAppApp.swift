89b393772c8e287bdd8e8741879b44859ec13fce
Himadri S Jyoti <himadri.sekhar.jyoti@walmart.com>
2021-04-10 14:54:43 +0530

    Simple project addition - ammended commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Himadri Sekhar Jyoti on 10/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
